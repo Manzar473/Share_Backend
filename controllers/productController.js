088617586e1821b50f62af3df101c78700c1b3d8
Manzar Abbas <abbasmanzar473@gmail.com>
2024-12-29 23:59:22 +0500

    updated get all products route

--- controllers/productController.js
+++ controllers/productController.js
@@ -42,7 +42,7 @@ exports.createProduct = async (req, res) => {
 // Get all products
 exports.getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find().populate('postedBy', 'username email'); // Populate postedBy with username and email
+        const products = await Product.find().populate('postedBy', 'username email area city'); // Populate postedBy with username and email
         res.status(200).json(products);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching products.', error: error.message });
